94cbc0be4aa7f0658e0189152913d45f74b4e449
pizzaYami <cyj9407@naver.com>
2023-08-28 12:38:19 +0900

    [feat] Navigation 완성

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -3,5 +3,7 @@ const list = document.querySelector(".list");
 const nav_container = document.querySelector(".nav_container");
 
 list.addEventListener("click", function () {
-  nav_container.classList.toggle("hidden");
+  nav_menus.classList.toggle("hidden");
+  nav_container.classList.toggle("size");
+  console.log(nav_menus);
 });
