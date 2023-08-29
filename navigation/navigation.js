28945f95ddb625ad292017abea306cc026b0874c
pizzaYami <cyj9407@naver.com>
2023-08-29 20:04:25 +0900

    [feat] modal 초기세팅

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -3,7 +3,6 @@ const list = document.querySelector(".list");
 const nav_container = document.querySelector(".nav_container");
 
 list.addEventListener("click", function () {
-  nav_menus.classList.toggle("hidden");
   nav_container.classList.toggle("size");
   console.log(nav_menus);
 });
