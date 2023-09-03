4c055fa5e2b2f144b418b29cd71ab8993c88a238
pizzaYami <cyj9407@naver.com>
2023-09-03 09:50:30 +0900

    [feat] html구조와 데이터

--- FAQ/FAQ.js
+++ FAQ/FAQ.js
@@ -1,32 +1,21 @@
-const countNumber = document.querySelector(".countNumber");
-const dec_bt = document.querySelector(".dec_bt");
-const reset_bt = document.querySelector(".reset_bt");
-const inc_bt = document.querySelector(".inc_bt");
-
-let number = 0;
-
-function changeColor() {
-  if (number < 0) {
-    countNumber.style.color = "red";
-  } else if (number === 0) {
-    countNumber.style.color = "black";
-  } else {
-    countNumber.style.color = "green";
-  }
-}
-
-function setCounterNumber(num) {
-  if (num !== 0) {
-    number = number + num;
-    countNumber.innerHTML = number;
-    changeColor();
-  } else {
-    number = 0;
-    countNumber.innerHTML = number;
-    changeColor();
-  }
-}
-
-dec_bt.addEventListener("click", () => setCounterNumber(-1));
-reset_bt.addEventListener("click", () => setCounterNumber(0));
-inc_bt.addEventListener("click", () => setCounterNumber(1));
+let data = [
+  {
+    id: 0,
+    Question: "How can I effectively manage stress?",
+    Answer:
+      "There are various ways to manage stress. Some methods include practicing meditation, deep breathing, and exercise. It's also important to manage your time efficiently, get enough rest, and find joy in small pleasures.",
+  },
+  {
+    id: 1,
+    Question: "What are some popular travel destinations recently?",
+    Answer:
+      "One of the currently popular travel destinations is Bali, Indonesia. Additionally, places like Santorini in Greece, Fukuoka in Japan, and Lisbon in Portugal are also considered popular travel destinations.",
+  },
+  {
+    id: 2,
+    Question:
+      "How is the advancement of artificial intelligence technology impacting different industries?",
+    Answer:
+      "The advancement of artificial intelligence technology is having a significant impact on various industries. For instance, in the medical field, it's being used to enhance diagnostic accuracy and develop personalized treatment approaches. In manufacturing, it's aiding in automation and increasing productivity. Moreover, in the financial sector, data analysis for predictions and automation is being actively pursued.",
+  },
+];
